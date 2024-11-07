366e1662f1bba1a7e928f67073b41926953ee9e3
Brandon Arias <2017030706@upsin.edu.mx>
2024-11-07 09:26:32 -0700

    Property section created

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,16 @@ $navToggler.addEventListener("click", () => {
 window.addEventListener("scroll", (e) => {
   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+/**
+ * Add to favorite button toggle
+ */
+
+const /** {NodeList} */ $toggleBtns =
+    document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () => {
+    $toggleBtn.classList.toggle("active");
+  });
+});
